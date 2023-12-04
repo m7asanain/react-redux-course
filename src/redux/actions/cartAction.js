415a63cb27872ba74415427011ad17ceb593097c
Mustafa Alkamali <m7asanain@gmail.com>
2023-12-04 20:35:46 +0300

    Redux Reducer Add To Cart

--- src/redux/actions/cartAction.js
+++ src/redux/actions/cartAction.js
@@ -1,6 +1,11 @@
+export const CART_ACTION_TYPES = {
+    ADD_TO_CART: 'ADD_TO_CART',
+    DELETE_FROM_CART: 'DELETE_FROM_CART'
+}
+
 export const addToCartAction = (product) => {
     return {
-        type: 'ADD_TO_CART',
+        type: CART_ACTION_TYPES.ADD_TO_CART,
         payload: product
     }
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,4 @@
+import { CART_ACTION_TYPES } from "../actions/cartAction";
 
 const INTIAL_STATE = {
     cart: [],
@@ -6,6 +7,21 @@ const INTIAL_STATE = {
 }
 
 const rootReducer = (state = INTIAL_STATE, action) => {
+    const { type, payload } = action;
+
+    if (type === CART_ACTION_TYPES.ADD_TO_CART) {
+        return {
+            ...state,
+            cart: state.cart.concat(payload),
+            cartTotal: state.cartTotal + payload.price
+        }
+    }
+
+    if (type === CART_ACTION_TYPES.DELETE_FROM_CART) {
+        return {
+            ...state,
+        }
+    }
     
     return {
         ...state,
